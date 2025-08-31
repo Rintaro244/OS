ac3c94e01a9a4f038f493cbb76b5608cc812a2c0
Rintaro <al24001@shibaura-it.ac.jp>
2025-08-31 20:12:32 +0900

    fix bootpack.hの内容修正

--- bootpack.h
+++ bootpack.h
@@ -163,7 +163,7 @@ void sheet_free(struct SHEET *sht);
 /* timer.c */
 #define MAX_TIMER		500
 struct TIMER {
-	struct TIMER *next_timer;
+	struct TIMER *next;
 	unsigned int timeout, flags;
 	struct FIFO32 *fifo;
 	int data;
